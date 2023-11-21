08d9e7eb30a9e5a1041025cc84995a102543e44f
robertoraciti <rob.raciti97@gmail.com>
2023-11-21 16:49:40 +0100

    add userseeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'User';
+        $user->surname = 'Admin';
+        $user->email = 'admin@email.it';
+        $user->password = Hash::make('password');
+        $user->save();
+    }
+}
