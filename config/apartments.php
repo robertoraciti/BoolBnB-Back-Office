911580563a6026fb0eb66a7ff54e9a84b68c443e
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-11-21 17:29:47 +0100

    added database for apartments

--- config/apartments.php
+++ config/apartments.php
@@ -3,10 +3,52 @@ return [
     [
         "name" => "Suite Susa",
         "description" => "New M4 under the house!!!
-        Cozy and bright open space overlooking Piazzale Susa. On the second floor of a building of the early 900 with elevator. the apartment of about 60sqm overlooks Piazzale Susa with pleasant views of the green and pretty balcony. New Metro M4 below the house!!!",
+        Cozy and bright open space overlooking Piazzale Susa. On the second floor of an ancient building.",
         "lat" => "45.4681",
         "lon" => '9.22382',
-
+        "address" => "Piazzale Susa, Milano"
+    ],
+    [
+        "name" => "Room with view, in Piazza Giuseppe Pasolini",
+        "description" => "Cozy place, with plenty of space and transportation nearby",
+        "lat" => "45.49624",
+        "lon" => '9.18597',
+        "address" => "Piazza Giuseppe Pasolini, Milano"
+    ],
+    [
+        "name" => "BB BnB near Vatican City",
+        "description" => "Lovely bnb, with european breakfast and close to the Vatican City",
+        "lat" => "41.90021",
+        "lon" => '9.18597',
+        "address" => "via dei cavalleggeri, Roma"
+    ],
+    [
+        "name" => "Cheap rooms close to Termini Station",
+        "description" => "Cheap rooms, with plenty of services, open space area for all the guests",
+        "lat" => "41.90559",
+        "lon" => '12.50202',
+        "address" => "via gaeta, Roma"
+    ],
+    [
+        "name" => "Lovely apartment, in Suffolk street",
+        "description" => "Lovely apartment, close to the theater and the national gallery ",
+        "lat" => "51.5084",
+        "lon" => '-0.13045',
+        "address" => "4 suffolk st, London"
+    ],
+    [
+        "name" => "Open space near St James's Square",
+        "description" => "Open space with lovely view of the square closeby and easy access to public transportation",
+        "lat" => "51.51181",
+        "lon" => '-0.12379',
+        "address" => "1 king street, London"
+    ],
+    [
+        "name" => "Open space near St James's Square",
+        "description" => "Open space with lovely view of the square closeby and easy access to public transportation",
+        "lat" => "51.51181",
+        "lon" => '-0.12379',
+        "address" => "1 king street, London"
     ],
 
 
