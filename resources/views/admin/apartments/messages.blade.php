7749186a3632890ba52e9dcb1dc25bf652c3597a
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-12-05 11:30:20 +0100

    added table for messages

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -7,5 +7,44 @@
 @endsection
 
 @section('content')
-    Messages
+<div class="container">
+
+    <div class="mt-4">
+        <h2>Messages for {{ $apartment->name }}</h2>
+    </div>
+
+    <table class="table mt-2">
+        <thead>
+            <tr>
+                <th scope="col">User</th>
+                <th scope="col">Email</th>
+                <th scope="col">Sent</th>
+                <th scope="col">Text</th>
+                {{-- <th scope="col">Actions</th> --}}
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($messages as $message)
+            <tr>
+                <td>{{ $message->name }}</td>
+                <td>{{ $message->email }}</td>
+                <td>{{ $message->created_at }}</td>
+                <td>{{ $message->text }}</td>
+                {{-- <td>
+                    <a href= " {{ route('admin.apartments.show', $apartment) }}"> <i
+                        class="fa-solid fa-eye"></i></a>
+                        <a href= " {{ route('admin.apartments.edit', $apartment) }}"> <i
+                            class="fa-solid fa-pencil mx-1 "></i> </a>
+                            <a href= "#" data-bs-toggle="modal"
+                            data-bs-target="#deleteModal-{{ $apartment->id }}"> <i
+                            class="fa-solid fa-trash text-danger"></i> </a>
+                        </td> --}}
+                    </tr>
+                    
+                @endforeach
+                {{ $messages->links('pagination::bootstrap-5') }}
+                    
+        </tbody>
+    </table>
+</div>
 @endsection
