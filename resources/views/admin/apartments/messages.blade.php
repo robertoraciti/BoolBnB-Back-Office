195e001871dbc42b52100aa6679097e039e46b51
Ericdid <ericdidom@gmail.com>
2023-12-07 17:46:07 +0100

    fix.details

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -7,32 +7,55 @@
 @endsection
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <div class="mt-4">
-        <h2>You have a total of <span class="badge text-bg-secondary">{{ $count }}</span> received messages for {{ $apartment->name }}</h2>
-        {{ $messages->links('pagination::bootstrap-5') }}
-    </div>
-
-    <div class="mt-4 container card-container">
-
-        @foreach ($messages as $message)
-        
-        <div class="card">
-            {{-- <img src="..." class="card-img-top" alt="..."> --}}
-            <div class="card-body">
-                <h5 class="card-title">User: {{ $message->name }}</h5>
-                <h5 class="card-title">Email: {{ $message->email }}</h5>
-                <h5 class="card-title">Sent: {{ $message->created_at }}</h5>
-                <h5 class="card-title">Message: </h5>
-                <p class="card-text">{{ $message->text }}</p>
-                
-            </div>
+        <div class="mt-4">
+            <h2 class="ms-style ms-rounded text-center"> You have a total of <span
+                    class=" ms-yellow">{{ $count }}</span> received
+                messages for {{ $apartment->name }}</h2>
+            {{ $messages->links('pagination::bootstrap-5') }}
         </div>
-        @endforeach
+
+        <div class="mt-4 container card-container">
+
+            @foreach ($messages as $message)
+                <div class="card my-3">
+                    {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                    <div class="card-body">
+                        <h5 class="card-title">User: {{ $message->name }}</h5>
+                        <h5 class="card-title">Email: {{ $message->email }}</h5>
+                        <h5 class="card-title">Sent: {{ $message->created_at }}</h5>
+                        <h5 class="card-title">Message: </h5>
+                        <p class="card-text">{{ $message->text }}</p>
+
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        {{ $messages->links('pagination::bootstrap-5') }}
+
+
     </div>
-    {{ $messages->links('pagination::bootstrap-5') }}
-                    
-      
-</div>
 @endsection
+
+
+<style>
+    .ms-style {
+        background-color: royalblue;
+        color: white;
+    }
+
+    .ms-rounded {
+        border-radius: 15px
+    }
+
+    .ms-yellow {
+        color: yellow;
+    }
+
+    .card:hover {
+        transform: translate3D(0, -1px, 0) scale(1.03);
+        box-shadow: 8px 14px 38px rgba(39, 44, 49, .06), 1px 3px 8px rgba(39, 44, 49, .03);
+        transition: all .5s ease;
+    }
+</style>
